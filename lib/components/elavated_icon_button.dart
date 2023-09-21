91977d014ffd8f4a11f5e2ad990b6ff7f2001041
yuu123456 <yuukunn3777@gmail.com>
2023-09-21 11:39:03 +0900

    elavated_icon_button作成

---
+++ lib/components/elavated_icon_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ElevatedIconButton extends StatelessWidget {
+
+  final String label;
+  final Color foregroundColor = Colors.black;
+  final Color backgroundColor = Colors.white;
+  final double fontSize = 14;
+  final FontWeight fontWeight = FontWeight.bold;
+  final bool disabled;
+  final Function? onPressed;
+  final double boderWidth = 2;
+  final Color boderColor = Colors.black;
+  final IconData iconData = Icons.sunny;
+
+  const ElevatedIconButton({super.key, required this.label, required this.disabled, this.onPressed,});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+  style: ElevatedButton.styleFrom(
+    foregroundColor: foregroundColor,
+    backgroundColor: backgroundColor,
+    textStyle: TextStyle(fontSize: fontSize, fontWeight: fontWeight,),
+    shape: StadiumBorder(side: BorderSide(width: boderWidth, color: boderColor),
+    ),
+  ),
+  onPressed: disabled ? null : () => onPressed,
+  icon: Icon(iconData),
+  label: Text(label),
+);
+  }
+
+}
