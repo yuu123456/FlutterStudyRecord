9240cb29894eea7944e91b31c82c4742fe11186f
yuu123456 <yuukunn3777@gmail.com>
2023-09-21 11:49:23 +0900

    flooting_action_button作成

---
+++ lib/components/flooting_action_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class FlootingActionButton extends StatelessWidget {
+
+  final String label;
+  final bool disabled;
+  final Function? onPressed;
+
+  const FlootingActionButton({super.key, required this.label, required this.disabled, this.onPressed,});
+
+  @override
+  Widget build(BuildContext context) {
+    return FlootingActionButton(
+  onPressed: disabled ? null : () => onPressed,
+  label: label, 
+  disabled: disabled,
+);
+  }
+
+}
