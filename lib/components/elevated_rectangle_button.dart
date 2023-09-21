7b4c33f43278b45590d1699fb096636fce6fa7a4
yuu123456 <yuukunn3777@gmail.com>
2023-09-21 11:32:14 +0900

    elevated_rectangle_button作成

---
+++ lib/components/elevated_rectangle_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ElevatedRectangleButton extends StatelessWidget {
+
+  final String label;
+  final Color foregroundColor = Colors.black;
+  final Color backgroundColor = Colors.white;
+  final double fontSize = 14;
+  final FontWeight fontWeight = FontWeight.bold;
+  final bool disabled;
+  final Function? onPressed;
+  final double boderWidth = 2;
+  final Color boderColor = Colors.black;
+  final double boderRadius = 8;
+
+  const ElevatedRectangleButton({super.key, required this.label, required this.disabled, this.onPressed,});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+  style: ElevatedButton.styleFrom(
+    foregroundColor: foregroundColor,
+    backgroundColor: backgroundColor,
+    textStyle: TextStyle(fontSize: fontSize, fontWeight: fontWeight,),
+    shape: RoundedRectangleBorder(side: BorderSide(width: boderWidth, color: boderColor,),borderRadius: BorderRadius.circular(boderRadius),
+    ),
+  ),
+  onPressed: disabled ? null : () => onPressed,
+  child: Text(label),
+);
+  }
+
+}
